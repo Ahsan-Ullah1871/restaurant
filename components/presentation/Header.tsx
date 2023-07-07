55940c58bd96cf280e6b786249c93ed4d1c95cc4
Ahsan-Ullah1871 <ahsanullahsunsbd@gmail.com>
2023-07-07 16:57:15 +0600

    test

--- components/presentation/Header.tsx
+++ components/presentation/Header.tsx
@@ -1,77 +1,50 @@
-import Link from "next/link";
-import Image from "next/image";
-import Logo from "@/Images/Logo.png";
-import Image from "next/image";
-import Image from "next/image";
+import Link from 'next/link';
+import Image from 'next/image';
+import Logo from '@/Images/Logo.png';
+import Image from 'next/image';
+import Image from 'next/image';
+import Image from 'next/image';
 
 const Header = () => {
-	return (
-		<div className=" max-w-project mx-auto flex items-center justify-between px-4">
-			{/* logo Image */}
-			<Image
-				src={Logo}
-				alt="Web logo"
-				width={75}
-				height={34}
-			/>
+  console.log('Here');
+  return (
+    <div className=" max-w-project mx-auto flex items-center justify-between px-4">
+      {/* logo Image */}
+      <Image src={Logo} alt="Web logo" width={75} height={34} />
 
-			{/* Menus */}
-			<div className="flex items-center justify-start gap-7">
-				<Link
-					href={"/"}
-					className="p3 text-grey"
-				>
-					About
-				</Link>
-				<Link
-					href={"/"}
-					className="p3 text-grey"
-				>
-					Menu
-				</Link>
-				<Link
-					href={"/"}
-					className="p3 text-grey"
-				>
-					Order Now
-				</Link>
-				<Link
-					href={"/"}
-					className="p3 text-grey"
-				>
-					Blog
-				</Link>
-				<Link
-					href={"/"}
-					className="p3 text-grey"
-				>
-					Contact
-				</Link>
-				<Link
-					href={"/"}
-					className="p3 text-grey"
-				>
-					Cart
-				</Link>
-			</div>
+      {/* Menus */}
+      <div className="flex items-center justify-start gap-7">
+        <Link href={'/'} className="p3 text-grey">
+          About
+        </Link>
+        <Link href={'/'} className="p3 text-grey">
+          Menu
+        </Link>
+        <Link href={'/'} className="p3 text-grey">
+          Order Now
+        </Link>
+        <Link href={'/'} className="p3 text-grey">
+          Blog
+        </Link>
+        <Link href={'/'} className="p3 text-grey">
+          Contact
+        </Link>
+        <Link href={'/'} className="p3 text-grey">
+          Cart
+        </Link>
+      </div>
 
-			<div className="flex items-center justify-end gap-7">
-				{/* sign in  */}
-				<Link
-					href={"/"}
-					className="button-2 text-dark"
-				>
-					Sign in
-				</Link>
+      <div className="flex items-center justify-end gap-7">
+        {/* sign in  */}
+        <Link href={'/'} className="button-2 text-dark">
+          Sign in
+        </Link>
 
-				{/*  */}
-				<button className="button-dark button-2">
-					Book a table
-				</button>
-			</div>
-		</div>
-	);
+        {/*  */}
+        <button className="button-dark button-2">Book a table</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
-
